639667617bcf92ca7389e27f3d0a7fc4da3fcece
Giovanni Spizzichino <spizzichinogiovanni@gmail.com>
2025-11-09 18:00:52 +0200

    DailyChallenge

--- week1/day1/DailyChallenge/Challenge.py
+++ week1/day1/DailyChallenge/Challenge.py
@@ -1,2 +1,21 @@
-sentence = input("Write a sentence of exactly 10 characters long ")
-len
+string = input("Write a sentence of exactly 10 characters long ")
+number_of_word = len(string)
+if number_of_word < 10:
+    print("String not long enough")
+elif number_of_word > 10:
+    print("String too long")
+else:
+    print("Perfect string")
+
+print(string[0:1], string[9:10])
+
+print(string[:1])
+print(string[:2])
+print(string[:3])
+print(string[:4])
+print(string[:5])
+print(string[:6])
+print(string[:7])
+print(string[:8])
+print(string[:9])
+print(string[:10])
