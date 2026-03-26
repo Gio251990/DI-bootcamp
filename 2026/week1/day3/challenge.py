639e0eae3efbf7c7d47d33cae84f9ef82e45889b
Giovanni Spizzichino <spizzichinogiovanni@gmail.com>
2026-03-26 18:00:35 +0200

    challenge

---
+++ 2026/week1/day3/challenge.py
@@ -0,0 +1,20 @@
+number = int(input("Please enter a number: "))
+length = int(input("Please enter a lenght: "))
+
+multiples = []
+
+for i in range(1, length + 1):
+    multiples.append(number * i)
+
+print(multiples)
+
+
+
+string = input("Enter a string: ")
+
+new_string = ""
+for i in range(len(string)):
+    if i == 0 or string[i] != string[i - 1]:
+        new_string += string[i]
+
+print(new_string)
