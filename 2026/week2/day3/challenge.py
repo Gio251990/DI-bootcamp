a79afa8d0172c4c4e51fc0d9dfefff1ec99de5fd
Giovanni Spizzichino <spizzichinogiovanni@gmail.com>
2026-04-13 12:28:07 +0300

    challenge

---
+++ 2026/week2/day3/challenge.py
@@ -0,0 +1,26 @@
+words = input("Enter words separated by commas: ")
+
+word_list = words.split(",")
+
+sorted_list = sorted(word_list)
+
+result = ",".join(sorted_list)
+
+print(result)
+
+
+def longest_word(sentence):
+    words = sentence.split()
+
+    longest = ""
+
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+
+    return longest
+
+
+print(longest_word("Margaret's toy is a pretty doll."))
+print(longest_word("A thing of beauty is a joy forever."))
+print(longest_word("Forgetfulness is by all means powerless!"))
