2b52e0284265f9dff0d2bc80c9a56eb8e9fb9ebe
Giovanni Spizzichino <spizzichinogiovanni@gmail.com>
2026-04-23 17:46:26 +0300

    challenge 1 e 2

---
+++ 2026/week2/day5/challenge2.js
@@ -0,0 +1,19 @@
+let stars = "";
+
+for (let i = 0; i < 6; i++) {
+    stars += "* ";
+    console.log(stars);
+}
+
+
+
+
+for (let i = 1; i <= 6; i++) {
+    let line = "";
+
+    for (let j = 0; j < i; j++) {
+        line += "* ";
+    }
+
+    console.log(line);
+}
